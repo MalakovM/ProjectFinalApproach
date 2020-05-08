de1426015550ec5286bc58e6d110b80c03f0a5d5
ReportMeX9 <477087@student.saxion.nl>
2020-05-08 17:23:25 +0200

    Added in new states for all muscle groups, updated some pictures used and added in a small bit of code that could maybe be used for pixel detection

--- GXPEngine/GXPEngine/ProjectFinalApproach/ProjectFinalApproach.cs
+++ GXPEngine/GXPEngine/ProjectFinalApproach/ProjectFinalApproach.cs
@@ -1,27 +1,38 @@
 using System;
 using System.Drawing;
+using GXPEngine.Core;
+using System.Collections.Generic;
 using GXPEngine;
 
 public class ProjectFinalApproach : Game
 {
+	bool isTurned;
 	Canvas _background;
-	Canvas _startMenu;
 	private State _state;
 	enum State
 	{
 		MAIN,
 		CHEST,
+		ABDOMEN,
+		BICEP,
+		TRICEP,
+		FOREARM,
+		SHOULDERS,
+		TRAPS,
+		HAMSTRINGS,
+		CALVES,
 		QUADS,
 		QUADSINFO,
 		MENU,
 		LIST,
+		BACK,
 	}
-	public ProjectFinalApproach():base (1920,1080, false, false)
+	public ProjectFinalApproach() : base(1920, 1080, false, false)
 	{
 		setState(State.MAIN);
 	}
 
-	
+
 	void handleState()
 	{
 		switch (_state)
@@ -46,46 +57,63 @@ public class ProjectFinalApproach : Game
 			case State.LIST:
 				handleList();
 				break;
+			case State.ABDOMEN:
+				handleAbdomen();
+				break;
+			case State.BACK:
+				handleBack();
+				break;
 		}
 	}
-	static void Main() {
-		new ProjectFinalApproach().Start ();
+	static void Main()
+	{
+		new ProjectFinalApproach().Start();
 	}
 	void Update()
 	{
 		Controls();
 		handleState();
+		//pixelColour();
 	}
 
 	void Controls()
 	{
 
 		//brings to chest
-		if ( Input.mouseX < 845 && Input.mouseX > 740 && Input.mouseY < 380 && Input.mouseY > 320 && _state != State.LIST)
+		if (Input.mouseX < 845 && Input.mouseX > 740 && Input.mouseY < 380 && Input.mouseY > 320 && _state != State.LIST && isTurned == false)
 		{
 			setState(State.CHEST);
 		}
 
 		//brings to quads
-		if (Input.mouseX < 820 && Input.mouseX > 735 && Input.mouseY < 560 && Input.mouseY > 495 && _state != State.QUADSINFO && _state != State.LIST)
+		if (Input.mouseX < 820 && Input.mouseX > 735 && Input.mouseY < 560 && Input.mouseY > 495 && _state != State.QUADSINFO && _state != State.LIST && isTurned == false)
 		{
 			setState(State.QUADS);
 		}
+		//abdomen
+		if (Input.mouseX < 820 && Input.mouseX > 735 && Input.mouseY < 560 && Input.mouseY > 495 && _state != State.LIST && isTurned == false)
+		{
+			setState(State.ABDOMEN);
+		}
 
-		if (Input.GetMouseButton(0) && Input.mouseX < 820 && Input.mouseX > 735 && Input.mouseY < 560 && Input.mouseY > 495 && _state != State.LIST)
+		if (Input.GetMouseButton(0) && Input.mouseX < 820 && Input.mouseX > 735 && Input.mouseY < 560 && Input.mouseY > 495 && _state != State.LIST && isTurned == false)
 		{
 			setState(State.QUADSINFO);
 		}
-
 		
-		if (Input.GetMouseButton(0) && Input.mouseX < 100 && Input.mouseX > 1 && Input.mouseY < 80 && Input.mouseY > 1 )
+
+		if (Input.GetMouseButton(0) && Input.mouseX < 100 && Input.mouseX > 1 && Input.mouseY < 80 && Input.mouseY > 1 && isTurned == false)
 		{
 			setState(State.MENU);
 		}
 
-		if (Input.GetMouseButton(0) && Input.mouseX < 180 && Input.mouseX > 1 && Input.mouseY < 120 && Input.mouseY > 80 && _state == State.MENU)
+		if (Input.GetMouseButton(0) && Input.mouseX < 180 && Input.mouseX > 1 && Input.mouseY < 120 && Input.mouseY > 80 && _state == State.MENU && isTurned == false)
 		{
 			setState(State.LIST);
+		}
+		if (Input.GetMouseButton(0) && Input.mouseX < 970 && Input.mouseX > 890 && Input.mouseY < 760 && Input.mouseY > 700 && _state != State.LIST && isTurned == false)
+		{
+			setState(State.BACK);
 		}
 	}
 	void setState(State newState)
@@ -101,13 +129,13 @@ public class ProjectFinalApproach : Game
 		//_background.SetOrigin(_background.width / 2, _background.height / 2);
 		//_background.SetXY(width / 2, height / 2);
 		AddChild(_background);
-		
+
 	}
 
 	void handleChestState()
 	{
 		_background.Destroy();
-		_background = new Canvas ("images/chestActive.png");
+		_background = new Canvas("images/chestActive.png");
 		AddChild(_background);
 	}
 
@@ -138,5 +166,35 @@ public class ProjectFinalApproach : Game
 		_background = new Canvas("images/exerciseList.png");
 		AddChild(_background);
 	}
-	
+
+	void handleBack()
+	{
+		_background.Destroy();
+		_background = new Canvas("images/backView.png");
+		AddChild(_background);
+		isTurned = true;
+	}
+
+	void handleAbdomen()
+	{
+		Console.WriteLine("Abdomen");
+	}
 }
+	/*
+	void pixelColour()
+	{
+		Color color = _background.texture.bitmap.GetPixel(100, 150);
+		if(color == Color.Blue)
+		{
+			_background.Destroy();
+			_background = new Canvas("images/exerciseList.png");
+			AddChild(_background); 
+			_background.InverseTransformPoint(Input.mouseX, Input.mouseY)
+		}
+		//mySprite <yourCodeMySprite>
+		//.texture.bitmap.GetPixel(100, 200);
+		//return mySprite;
+
+		//return averageColor
+	}
+	*/
